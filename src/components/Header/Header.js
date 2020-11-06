dff551449a8c4a665abcc7c92ad2190130a03ebe
Shahjahan Md Swajan <swajan.mis13.du@gmail.com>
2020-11-06 10:18:48 +0600

    Bugfixed seo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,54 +17,57 @@ const Header = () => {
     };
     return (
         <header>
-            <Navbar variant="white" className="py-3">
+            <Navbar collapseOnSelect expand="lg" variant="white" className="py-3">
                 <Container>
                     <Navbar.Brand className="mr-auto" href="/">
                         <img className="site-logo" src={LogoImg} alt="" />
                     </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav text-center">
 
-                    <Nav className="d-flex align-items-center text-dark">
-                        <Nav.Link href="/">
-                            <span className="text-dark"> Home</span>
-                        </Nav.Link>
-                        <Nav.Link href="/">
-                            <span className="text-dark">Register New</span>
-                        </Nav.Link>
-                        {currentUser && (
-                            <Nav.Link href={"/myevents/" + currentUser.email}>
-                                <span className="text-dark"> My Events</span>
-                            </Nav.Link>
-                        )}
-
-                        <Nav.Link href="/">
-                            <span className="text-dark"> Blog</span>
-                        </Nav.Link>
-
-                        <Nav.Link href="/admin">
-                            <span className="btn btn-dark">Admin</span>
-                        </Nav.Link>
-                        {currentUser && (
+                        <Nav className="d-flex align-items-center text-dark">
                             <Nav.Link href="/">
-                                <span className="btn btn-primary">
-                                    {currentUser.name}
-                                </span>
+                                <span className="text-dark"> Home</span>
                             </Nav.Link>
-                        )}
-                        {currentUser ? (
-                            <Nav.Link>
-                                <button
-                                    onClick={() => handleSignOut()}
-                                    className="btn btn-danger"
-                                >
-                                    LogOut
-                                </button>
+                            <Nav.Link href="/">
+                                <span className="text-dark">Register New</span>
                             </Nav.Link>
-                        ) : (
-                                <Nav.Link href="/">
-                                    <span className="btn btn-primary">Login</span>
+                            {currentUser && (
+                                <Nav.Link href={"/myevents/" + currentUser.email}>
+                                    <span className="text-dark"> My Events</span>
                                 </Nav.Link>
                             )}
-                    </Nav>
+
+                            <Nav.Link href="/">
+                                <span className="text-dark"> Blog</span>
+                            </Nav.Link>
+
+                            <Nav.Link href="/admin">
+                                <span className="btn btn-dark">Admin</span>
+                            </Nav.Link>
+                            {currentUser && (
+                                <Nav.Link href="/">
+                                    <span className="btn btn-primary">
+                                        {currentUser.name}
+                                    </span>
+                                </Nav.Link>
+                            )}
+                            {currentUser ? (
+                                <Nav.Link>
+                                    <button
+                                        onClick={() => handleSignOut()}
+                                        className="btn btn-danger"
+                                    >
+                                        LogOut
+                                </button>
+                                </Nav.Link>
+                            ) : (
+                                    <Nav.Link href="/">
+                                        <span className="btn btn-primary">Login</span>
+                                    </Nav.Link>
+                                )}
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </header>
